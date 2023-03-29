5b5b861e74724d9479457262a4dbb6ae04af40a7
Paweł Mroczka <pawel.mroczka@neti-soft.comm>
2023-03-29 12:55:48 +0200

    Add comments and optimize funcs

--- contracts/LockBooster.sol
+++ contracts/LockBooster.sol
@@ -1,4 +1,4 @@
- // SPDX-License-Identifier: None
+// SPDX-License-Identifier: None
 
 pragma solidity ^0.8.9;
 
@@ -11,20 +11,21 @@ import {IMasterChefAdmin} from "./interfaces/IMasterChefAdmin.sol";
 
 contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
     // Internal constant variables
-    bytes32 public constant BOOST_MANAGER_ROLE =  keccak256("BOOST_MANAGER_ROLE");
+    bytes32 public constant BOOST_MANAGER_ROLE =
+        keccak256("BOOST_MANAGER_ROLE");
 
     uint256 internal constant DENOMINATOR = 1e10;
-    uint256 public monthlyBoost;
-    uint256 public yearlyBoost;
+    uint256 public monthlyBoost = 2500;
+    uint256 public yearlyBoost = 300000;
     ITokenLocker public tokenLocker;
     IMasterChefAdmin public masterChefAdmin;
-    mapping(address => User) public boostedUser;  
+    mapping(address => User) public boostedUser;
 
     // Struct for each boosted user
     struct User {
-          uint256 boost;
-          uint256 pid;
-      		bool init;
+        uint256 boost;
+        uint256 pid;
+        bool init;
     }
 
     // Error handling
@@ -55,62 +56,79 @@ contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
         require(_newaddress != address(0), "New addr role is zero.");
         _;
     }
-
-    /// @notice gas optimization by assigning the boosters in constructor
-      constructor() {
-        monthlyBoost = 2500; // 25e2
-        yearlyBoost = 300000; // 3e5
+    /**
+    * @dev external function for users
+    */
+    function updateBoost(uint256 _pid) external {
+        _updateBoost(msg.sender, _pid);
     }
-      /**
-        * @notice Updates the boost multiplier for the specified account and pool ID in MasterChef
-        * @dev Boost is calculated based on the account's locked tokens in the LockerContract.
-        *  If the account is already boosted in the specified pool, the boost multiplier is updated to the new value.
-        * @param _account The account to update the boost multiplier for.
-        * @param _pid The ID of the pool to update the boost multiplier for.
-        */
-    function updateBoost(address _account, uint256 _pid) external onlyOwner {
-        uint256 boost = (boostLock(_account) + 100 * 1e6) * 1e4;
-        if (_pid == boostedUser[_account].pid) {
+
+     /**
+    * @dev external function for boostManager
+    */
+    function updateBoostManager(address _account, uint256 _pid) external onlyBoostManager {
+      _updateBoost(_account, _pid);
+    }
+
+
+    /**
+     * @notice Updates the boost multiplier for the specified account and pool ID in MasterChef
+     * @dev Boost is calculated based on the account's locked tokens in the LockerContract.
+     *  If the account is already boosted in the specified pool, the boost multiplier is updated to the new value.
+     * @param _account The account to update the boost multiplier for.
+     * @param _pid The ID of the pool to update the boost multiplier for.
+     */
+    function _updateBoost(address _account, uint256 _pid) internal{
+        uint256 boost = (calculateUserBoost(_account) + 100 * 1e6) * 1e4;
+        User storage user = boostedUser[_account];
+        if (_pid == user.pid) {
             masterChefAdmin.updateBoostMultiplier(_account, _pid, boost);
         } else {
-          if(boostedUser[_account].init){
-              masterChefAdmin.updateBoostMultiplier(
-                _account,
-                boostedUser[_account].pid,
-                100 * 1e10
-            );
-          }else{
-            boostedUser[_account].init = true;
-          }
+            if (user.init) {
+                masterChefAdmin.updateBoostMultiplier(_account, user.pid, 100 * 1e10);
+            } else {
+                user.init = true;
+            }
             masterChefAdmin.updateBoostMultiplier(_account, _pid, boost);
-            boostedUser[_account].pid = _pid;
+            user.pid = _pid;
         }
+        user.boost = boost;
     }
 
-   function getUsersBoost(address[] memory _users) public view returns (User[] memory) {
-        User[] memory users = new User[](_users.length); 
+    /**
+     * @dev Function returns users information
+     * @param _users an array of accounts to get information for.
+     */
+    function getUsersBoost(
+        address[] calldata _users
+    ) external view returns (User[] memory) {
+        User[] memory users = new User[](_users.length);
         for (uint256 i = 0; i < _users.length; i++) {
-        users[i] = boostedUser[_users[i]];
+            users[i] = boostedUser[_users[i]];
         }
         return users;
     }
-     
-	/*
-    function getUsersBoost(address[] memory _users) public view returns (address[] memory addressArray, uint256[] memory boostArray, uint256[] memory pidArray, bool[] memory initArray) {
-        addressArray = new address[](_users.length);
-        boostArray = new uint256[](_users.length);
-        pidArray = new uint256[](_users.length);
-        initArray = new bool[](_users.length);
+
+    /**
+     * @notice Check boost changes and updates the boost multiplier for accounts in array
+     * @dev Checking boost is based on value saved in user info and actual calculation.
+     *  If the boost is different, function updates boost on already boosted pool
+     * @param _users The account to update the boost multiplier for.
+     */
+
+    function chceckAndUpdateUsersLocks(address[] memory _users) external {
         for (uint256 i = 0; i < _users.length; i++) {
-            addressArray[i] = _users[i];
-            boostArray[i] = boostedUser[_users[i]].boost;
-            pidArray[i] = boostedUser[_users[i]].pid;
-            initArray[i] = boostedUser[_users[i]].init;
+            if (boostedUser[_users[i]].boost != calculateUserBoost(_users[i])) {
+                _updateBoost(_users[i], boostedUser[_users[i]].pid);
+            }
         }
     }
-    */
-  
-    function boostLock(address _account) public view returns (uint256 boost) {
+
+    /**
+     * @dev Boost is calculated based on the account's locked tokens in the LockerContract.
+     * @param _account The account to calculate the boost multiplier for.
+     */
+    function calculateUserBoost(address _account) public view returns (uint256 boost) {
         LibPausable.enforceNotPaused();
         require(
             address(tokenLocker) != address(0),
@@ -126,24 +144,41 @@ contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
             365 days
         );
         boost = 0;
-        if (monthLock.duration > 0 && monthLock.amount > 0) {
+        if (
+            monthLock.duration > 0 &&
+            monthLock.amount > 0 &&
+            monthLock.expiresAt >= block.timestamp
+        ) {
             // struct validation
-            boost += calculateBoost(monthLock.amount, monthLock.duration);
+            boost += _calculateBoost(monthLock.amount, monthLock.duration);
         }
-        if (yearLock.duration > 0 && yearLock.amount > 0) {
+        if (
+            yearLock.duration > 0 &&
+            yearLock.amount > 0 &&
+            yearLock.expiresAt >= block.timestamp
+        ) {
             // struct validation
-            boost += calculateBoost(yearLock.amount, yearLock.duration);
+            boost += _calculateBoost(yearLock.amount, yearLock.duration);
         }
         return boost >= 15e16 ? 15e16 / DENOMINATOR : boost / DENOMINATOR;
     }
 
-    function calculateBoost(
+    /**
+     * @dev Calculating boost based on given amount and duration
+     * @param duration, time period of token locking
+     * @param amount, amount of locked KSWAP tokens
+     */
+    function _calculateBoost(
         uint256 amount,
         uint32 duration
     ) internal view returns (uint256 rewardAmount) {
         return (amount * getBoostRate(duration)) / DENOMINATOR;
     }
 
+    /**
+     * @dev Calculating boost rate based on given duration
+     * @param duration, time period of token locking
+     */
     function getBoostRate(
         uint32 duration
     ) internal view returns (uint256 boostRate) {
@@ -152,6 +187,13 @@ contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
         else revert UnsupportedDuration();
     }
 
+    /**
+     * @dev Initialization of the contract
+     * @param _owner, address of contract owner
+     * @param _boostManager, address of boost manager
+     * @param _tokenLocker, address of tokenLocker contract
+     * @param _masterChef, address of masterChef contract
+     */
     function initialize(
         address _owner,
         address _boostManager,
@@ -170,11 +212,19 @@ contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
         LibAccessControl.grantRole(LibRoles.DEFAULT_ADMIN_ROLE, _owner);
     }
 
+    /**
+     * @dev Changing access address tokenLocker contract
+     * @param _tokenLocker new contract address
+     */
     function changeLockerAddr(ITokenLocker _tokenLocker) external onlyOwner {
         require(address(_tokenLocker) != address(0));
         tokenLocker = _tokenLocker;
     }
 
+    /**
+     * @dev Changing access address MasterChef contract
+     * @param _masterChefAdmin new contract address
+     */
     function changeMasterChefAddr(
         IMasterChefAdmin _masterChefAdmin
     ) external onlyOwner {
@@ -182,6 +232,11 @@ contract LockerBooster is AccessControlFacet, PausableFacet, Initializer {
         masterChefAdmin = _masterChefAdmin;
     }
 
+    /**
+     * @dev Changing address for BOOST_MANAGER_ROLE
+     * @param _oldBoostManager, current address to revoke role
+     * @param _newBoostManager, new addres to grant role
+     */
     function changeBoostManager(
         address _oldBoostManager,
         address _newBoostManager
