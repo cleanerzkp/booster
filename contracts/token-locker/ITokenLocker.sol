600c69474310138cf041c203d728a742cb700484
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-04-03 23:36:20 +0200

    Updated files for testing

--- contracts/token-locker/ITokenLocker.sol
+++ contracts/token-locker/ITokenLocker.sol
@@ -1,63 +1,12 @@
-// SPDX-License-Identifier: MIT
-
-////////////////////////////////////////////////solarde.fi//////////////////////////////////////////////
-//_____/\\\\\\\\\\\_________/\\\\\_______/\\\_________________/\\\\\\\\\_______/\\\\\\\\\_____        //
-// ___/\\\/////////\\\_____/\\\///\\\____\/\\\_______________/\\\\\\\\\\\\\___/\\\///////\\\___       //
-//  __\//\\\______\///____/\\\/__\///\\\__\/\\\______________/\\\/////////\\\_\/\\\_____\/\\\___      //
-//   ___\////\\\__________/\\\______\//\\\_\/\\\_____________\/\\\_______\/\\\_\/\\\\\\\\\\\/____     //
-//    ______\////\\\______\/\\\_______\/\\\_\/\\\_____________\/\\\\\\\\\\\\\\\_\/\\\//////\\\____    //
-//     _________\////\\\___\//\\\______/\\\__\/\\\_____________\/\\\/////////\\\_\/\\\____\//\\\___   //
-//      __/\\\______\//\\\___\///\\\__/\\\____\/\\\_____________\/\\\_______\/\\\_\/\\\_____\//\\\__  //
-//       _\///\\\\\\\\\\\/______\///\\\\\/_____\/\\\\\\\\\\\\\\\_\/\\\_______\/\\\_\/\\\______\//\\\_ //
-//        ___\///////////__________\/////_______\///////////////__\///________\///__\///________\///__//
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+// SPDX-License-Identifier: None
 pragma solidity ^0.8.9;
 
 interface ITokenLocker {
     struct Lock {
         uint256 amount;
-        uint256 reward;
         uint32 duration;
-        uint32 lockedAt;
-        uint32 expiresAt;
+        uint64 expiresAt;
     }
 
-    error UnsupportedDuration();
-    error LockNotFound();
-    error LockNotExpired();
-
-    event Deposit(
-        address account,
-        uint256 amount,
-        uint256 reward,
-        uint32 duration,
-        uint32 lockedAt,
-        uint32 expiresAt
-    );
-
-    event Redeem(
-        address account,
-        uint256 amount,
-        uint256 reward,
-        uint32 duration,
-        uint32 lockedAt
-    );
-
-    function deposit(uint256 amount, uint32 duration) external;
-
-    function redeem(uint32 duration) external;
-
-    function getLock(
-        address account,
-        uint32 duration
-    ) external view returns (Lock memory lock);
-
-    /// @notice DEPRECATED
-    /// @dev Used in the first version. Was migrated to new logic.
-    struct UserInfo {
-        uint256 amount;
-        uint32 duration;
-        uint32 lockedAt;
-    }
+    function getLock(address _user, uint32 _duration) external view returns (Lock memory);
 }
